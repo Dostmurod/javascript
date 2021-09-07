34cb5913db8da7b8974d3a1a5183c5579688cb8e
Dostmurod <dostmurodzoirov777@gmail.com>
2021-09-07 21:38:39 +0500

    javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+var dostmurodboyi = 30;
+        if (dostmurodboyi >=90 ) {
+            console.log("uning boyi A");
+
+        }
+
+
+        else if (dostmurodboyi < 90 && dostmurodboyi >= 80) {
+            console.log ("uning boyi B")
+        }
+        else if (dostmurodboyi < 80 && dostmurodboyi >= 70) {
+            console.log ("uning boyi c")
+        }
+        else if (dostmurodboyi < 70 && dostmurodboyi >= 60) {
+            console.log ("uning boyi d")
+        }
+        else{
+            console.log ("unig boyi juda past")
+        }
---
+++ index.js
@@ -0,0 +1,26 @@
+//let box = "dostmurod00"
+//console.log(box)
+//console.log(box)
+
+//let names = "bekmurod"
+//console.log(names)
+//console.log(names)
+//let pro = +prompt("Yoshingizni Kiriting nechanchi yilda tug'ilganingizni bilib beraman")
+//alert (2021-pro);
+//let sultan = confirm ("Sultonmurod")
+//console.log (sultan)
+//let arr = ["aplle",45,"iPhone"]
+//arr.push("arr")
+//arr.pop("iPhone")
+//arr.shift("iPhone")
+//arr.unshift("aplle")
+
+//console.log(arr)
+
+//let lesson = prompt["nok","anor","shaftmoli","o'rik","gilos","olma","iPhone"]
+//alert(01234567 -lesson)
+//console.log(lesson)
+
+let sovga = +prompt (["Siz 0 dan 10 gacha bo'lgan raqamni kiriting ular ostida yutuq kutmoqda sizni"])
+alert ("Tabriklaymiz Sizning yutug'ingiz Nok"," Tabriklaymiz Sizning yutug'ingiz anor","Tabriklaymiz Sizning yutug'ingiz banan","Tabriklaymiz Sizning yutug'ingiz uzum","Tabriklaymiz Sizning yutug'ingiz shaftoli","Tabriklaymiz Sizning yutug'ingiz gilos","Tabriklaymiz Sizning yutug'ingiz o'rik","Tabriklaymiz Sizning yutug'ingiz behi","Tabriklaymiz Sizning yutug'ingiz xurmo","Tabriklaymiz Sizning yutug'ingiz olma","Tabriklaymiz Sizning yutug'ingiz malina" + sovga)
+console.log(sovga)
